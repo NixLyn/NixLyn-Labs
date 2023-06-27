7d8ff5e710ce133bea2ceb045619453f62dcfb66
dillonbreytenach <dillon.nixlyn@gmail.com>
2023-06-27 04:04:27 -0500

    Update: 2023-June-27@11:00

--- nixlynlabs/nixlynlab/settings.py
+++ nixlynlabs/nixlynlab/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7$j0*48kgbno7e#wnam19ypuk&*x7^aq1@pt&&zucnd+1h1w55
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'nixlyn.com',
+    '*',
 ]
 
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddlewear',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -52,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'nixlynlab.urls'
+ROOT_URLCONF = 'nixlynlabs.urls'
 
 TEMPLATES = [
     {
@@ -118,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")] 
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
